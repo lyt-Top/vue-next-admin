80b3bed4d287ccab24dfd6e2dd65adbe017cf4cf
lyt-Top <1105290566@qq.com>
2021-05-16 17:33:17 +0800

    'mould-21.05.16:添加eslint规则、更新最新依赖'

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,12 @@ module.exports = {
 	rules: {
 		// http://eslint.cn/docs/rules/
 		// https://eslint.vuejs.org/rules/
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/no-inferrable-types': 'off',
+		'vue/no-setup-props-destructure': 'off',
+		'no-prototype-builtins': 'off',
 	},
 };
