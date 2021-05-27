6386c2b952eabe7080de32b15e37d915bfaa40ff
lyt-Top <1105290566@qq.com>
2021-05-27 21:15:26 +0800

    'admin-21.05.27:新增utils下的storage.js读取浏览器缓存新写法，建议使用'

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -35,3 +35,46 @@ export function removeSession(key) {
 export function clearSession() {
 	window.sessionStorage.clear();
 }
+
+// 新写法，简单易记，建议使用
+// 1、window.localStorage   浏览器永久缓存
+export const Local = {
+	// 设置永久缓存
+	set(key, val) {
+		window.localStorage.setItem(key, JSON.stringify(val));
+	},
+	// 获取永久缓存
+	get(key) {
+		let json = window.localStorage.getItem(key);
+		return JSON.parse(json);
+	},
+	// 移除永久缓存
+	remove(key) {
+		window.localStorage.removeItem(key);
+	},
+	// 移除全部永久缓存
+	clear() {
+		window.localStorage.clear();
+	},
+};
+
+// 2、window.sessionStorage 浏览器临时缓存
+export const Session = {
+	// 设置临时缓存
+	set(key, val) {
+		window.sessionStorage.setItem(key, JSON.stringify(val));
+	},
+	// 获取临时缓存
+	get(key) {
+		let json = window.sessionStorage.getItem(key);
+		return JSON.parse(json);
+	},
+	// 移除临时缓存
+	removeLocal(key) {
+		window.sessionStorage.removeItem(key);
+	},
+	// 移除全部临时缓存
+	clear() {
+		window.sessionStorage.clear();
+	},
+};
