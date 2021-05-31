2bc69ff31f20af3ef0c7339940e296116190fa23
lyt-Top <1105290566@qq.com>
2021-05-31 22:14:51 +0800

    'admin-21.05.31:修复分栏、经典布局路由设置meta.isHide为true时报错问题，感谢群友@29、@芭芭拉'

---
+++ src/views/personal/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="personal" :style="{ height: `calc(100vh - ${setMainHeight}` }">
+		<div class="layout-view-bg-white flex">
+			<div class="flex-margin color-primary">personal</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'personal',
+	data() {
+		return {};
+	},
+	computed: {
+		// 设置主盒子高度
+		setMainHeight() {
+			let { isTagsview } = this.$store.state.themeConfig.themeConfig;
+			if (isTagsview) return `114px`;
+			else return `80px`;
+		},
+	},
+};
+</script>
+
+<style scoped lang="scss">
+.personal  {}
+</style>
