161748a549664812c3dded853f20d92ffa5bfdbb
lyt <1105290566@qq.com>
2022-11-19 00:09:19 +0800

    'admin-22.11.19:修复v2.3.0版本动态路由事件调用关闭当前tagsview、普通路由刷新界面参数丢失问题'

--- src/router/index.ts
+++ src/router/index.ts
@@ -114,11 +114,12 @@ router.beforeEach(async (to, from, next) => {
 					// 后端控制路由：路由数据初始化，防止刷新时丢失
 					await initBackEndControlRoutes();
 					// 解决刷新时，一直跳 404 页面问题，关联问题 No match found for location with path 'xxx'
-					next({ path: to.path });
+					// to.query 防止页面刷新时，普通路由带参数时，参数丢失。动态路由（xxx/:id/:name"）isDynamic 无需处理
+					next({ path: to.path, query: to.query });
 				} else {
 					// https://gitee.com/lyt-top/vue-next-admin/issues/I5F1HP
 					await initFrontEndControlRoutes();
-					next({ path: to.path });
+					next({ path: to.path, query: to.query });
 				}
 			} else {
 				next();
