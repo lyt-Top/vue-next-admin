a67f379a4d1182765a510b0775d39b20d8290be0
lyt <1105290566@qq.com>
2023-03-10 23:17:26 +0800

    'admin-23.03.10:同步master分支v2.4.31版本内容'

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue';
 import pinia from '/@/stores/index';
-import App from './App.vue';
-import router from './router';
+import App from '/@/App.vue';
+import router from '/@/router';
 import { directive } from '/@/directive/index';
 import other from '/@/utils/other';
 
 import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
 import '/@/theme/index.scss';
 
 const app = createApp(App);
